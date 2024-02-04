5cd18e84d4c3dfed93b22fcfd6ffdbb9b29eb75e
Abdulrahim Muhamed <abdelrheem2204@gmail.com>
2024-02-04 21:13:40 +0200

    week3

--- Assignments/BOM.js
+++ Assignments/BOM.js
@@ -33,3 +33,80 @@ colors.forEach( color => {
 
 
 // ################################################################################################
+function popUp() {
+  let popUpContainer = document.createElement('div');
+  popUpContainer.style = `
+    width: 400px;
+    height: 150px;
+    background-color: #ddd;
+    border-radius: 1px;
+    position: fixed;
+    top: 35%;
+    left: 35%;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+  `;
+  popUpContainer.onclick = () => {
+    document.body.removeChild(popUpContainer);
+  }
+
+  let closePopUp = document.createElement('div');
+  closePopUp.textContent = 'x'
+  closePopUp.style = `
+    background-color: red;
+    border-radius: 0 0 1px 0;
+    font-size: 18px;
+    font-weight: bold;
+    color: white;
+    cursor: pointer;
+    padding: 0 7px 3px;
+    position: fixed;
+    top: 35%;
+    right: 39%;
+  `;
+
+  let popUpTitle = document.createElement('h1');
+  popUpTitle.textContent = 'Welcome';
+
+  let popUpMessage = document.createElement('p');
+  popUpMessage.textContent = 'Welcome to Abdulrahim Popup';
+  popUpMessage.style = `
+    margin-top: 20px;
+    font-size: 18px;
+  `;
+
+  popUpContainer.appendChild(popUpTitle);
+  popUpContainer.appendChild(popUpMessage);
+  popUpContainer.appendChild(closePopUp);
+  document.body.appendChild(popUpContainer)
+}
+
+onload = popUp();
+
+
+// ################################################################################################
+let count = document.querySelector('div');
+
+let timerIndex = setInterval( () => {
+  if (count.textContent == 1){
+    clearInterval(timerIndex);
+  }
+    --count.textContent
+}, 1000);
+
+
+// ################################################################################################
+let count1 = document.querySelector('div');
+
+let timerIndex1 = setInterval( () => {
+  if (count1.textContent == 5){
+    window.open('https://Elzero.org', '_blank', 'width=500,height=400,top=0,left=0');
+    clearInterval(timerIndex1)
+  }
+    --count1.textContent
+}, 1000);
+
+
+// ################################################################################################
--- testing.js
+++ testing.js
@@ -1,48 +1,28 @@
-function popUp() {
-  let popUpContainer = document.createElement('div');
-  popUpContainer.style = `
-    width: 400px;
-    height: 150px;
-    background-color: #ddd;
-    border-radius: 1px;
-    position: fixed;
-    top: 35%;
-    left: 35%;
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-  `;
+let fontFamily = document.getElementById('font');
+let fontSize = document.getElementById('size');
+let color = document.getElementById('color');
 
-  let closePopUp = document.createElement('div');
-  closePopUp.textContent = 'x'
-  closePopUp.style = `
-    background-color: red;
-    border-radius: 0 0 1px 0;
-    font-size: 18px;
-    font-weight: bold;
-    color: white;
-    cursor: pointer;
-    padding: 0 7px 3px;
-    position: fixed;
-    top: 35%;
-    right: 39%;
-  `;
+fontFamily.value = localStorage.getItem('font family');
+fontSize.value = localStorage.getItem('font size');
+color.value = localStorage.getItem('color');
 
-  let popUpTitle = document.createElement('h1');
-  popUpTitle.textContent = 'Welcome';
+document.body.style = `
+  font-family: ${localStorage.getItem('font family') ? localStorage.getItem('font family') : fontFamily.value};
+  font-size: ${localStorage.getItem('font size') ? localStorage.getItem('font size') : fontSize.value}px;
+  color: ${localStorage.getItem('color') ? localStorage.getItem('color') : color.value};
+`;
 
-  let popUpMessage = document.createElement('p');
-  popUpMessage.textContent = 'Welcome to Abdulrahim Popup';
-  popUpMessage.style = `
-    margin-top: 20px;
-    font-size: 18px;
-  `;
+fontFamily.addEventListener('change', (e) => {
+  localStorage.setItem('font family', e.target.value);
+  document.body.style.fontFamily = localStorage.getItem('font family');
+});
 
-  popUpContainer.appendChild(popUpTitle);
-  popUpContainer.appendChild(popUpMessage);
-  popUpContainer.appendChild(closePopUp);
-  document.body.appendChild(popUpContainer)
-}
+fontSize.addEventListener('change', (e) => {
+  localStorage.setItem('font size', e.target.value);
+  document.body.style.fontSize = localStorage.getItem('font size') + 'px';
+});
 
-onload = popUp();
+color.addEventListener('change', (e) => {
+  localStorage.setItem('color', e.target.value);
+  document.body.style.color = localStorage.getItem('color');
+});
